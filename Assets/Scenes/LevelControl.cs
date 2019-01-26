1410e076d1f51759cd9e57acff0b3517a7ee10b7
Chimachu <47048946+Chimachu@users.noreply.github.com>
2019-01-26 01:05:16 -0600

    SceneManagement

---
+++ Assets/Scenes/LevelControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelControl : MonoBehaviour
+{
+    public string LevelName;
+    public void OnTriggerEnter2D(Collider2D Battle)
+    {
+        if (Battle.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(LevelName);
+        }
+    }
+    public void GoToNewScene()
+    {
+        SceneManager.LoadScene(LevelName);
+    }
+
+}

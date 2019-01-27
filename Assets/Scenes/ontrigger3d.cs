3b901bdf191a40239d0907c1d131891e1a813fb9
rachelgroth <38136112+rachelgroth@users.noreply.github.com>
2019-01-27 12:27:10 -0600

    Fix build

---
+++ Assets/Scenes/ontrigger3d.cs
@@ -0,0 +1,27 @@
+﻿using Fungus;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ontrigger3d : MonoBehaviour
+{
+    public Flowchart flow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Flowchart.BroadcastFungusMessage("BossEncounter");
+        flow.ExecuteBlock("Trigger");
+    }
+}
